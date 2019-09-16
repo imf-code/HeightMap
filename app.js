3adaa7d5032cea0adfda3b6cede3256cf95e8c34
Ile <atroo_000@ILELPPRI>
2019-09-16 13:44:24 +0300

    No more node, now uses HTML

--- app.js
+++ app.js
@@ -3,26 +3,30 @@
 const fs = require('fs');
 
 //Used variables
-var rawData;
-var numOnly;
+var rawData = "";
+var numOnly = "";
 var mapArray;
-var printOut;
 
 
 //Read data from file, get number values
-rawData = fs.readFileSync('L3324.asc', 'ascii');
+console.log("Reading file...");
+rawData = fs.readFileSync('Testi2.asc', 'ascii');
 numOnly = rawData.slice(157);
+console.log("Done.");
 
 
 //Turn into 2D string array
+console.log("Converting to array...");
 mapArray = numOnly.split("\n")
 for (let i = 0; i < mapArray.length; i++) {
     mapArray[i] = mapArray[i].split(" ");
 }
 mapArray.pop();
+console.log("Done.");
 
 
 //Convert into float
+console.log("Converting to float...");
 function IntoFloat(item, index, arr) {
     arr[index] = parseFloat(item);
 }
@@ -32,9 +36,11 @@ function DIntoFloat(item, index, arr) {
 }
 
 mapArray.forEach(DIntoFloat);
+console.log("Done.");
 
 
 //Write boolMap
+console.log("Creating boolMap...")
 var boolMap = new Array(mapArray.length);
 for (let row = 0; row < mapArray.length; row++) {
     boolMap[row] = [];
@@ -46,15 +52,16 @@ for (let row = 0; row < mapArray.length; row++) {
             boolMap[row][column] = "0";
         }
     }
-    mapArray[row].push("\n");
+    //mapArray[row].push("\n");
 }
+console.log("Done.")
 
-
-//Print the array into a string
-for (let row = 0; row < boolMap.length; row++) {
+//Print an array to console
+var targetVar = boolMap;
+for (let row = 0; row < targetVar.length; row++) {
     //printOut = boolMap[row].join("");
 
-    console.log(...boolMap[row]); //Direct print (slow?)
+    console.log(...targetVar[row]); //Direct print (slow?)
 
     /* Old way
     for (let column = 0; column < mapArray[row].length; column++) {
@@ -63,24 +70,16 @@ for (let row = 0; row < boolMap.length; row++) {
     */
 }
 
-console.log(printOut);
+//console.log(printOut);
 
-
-//Remove empty indexes, may or may not be needed
-/*
-function RemEmpty(empty) {
-    return empty != "";
+/*Remove newline indexes (may or may not be needed)
+function NoNewline(newline) {
+    return newline != "\n";
 }
-mapArray[0] = mapArray[0].filter(RemEmpty);
-*/
 
-/*Check if above sea level
-function CheckIfAbove(item, index, arr) {
-    if (item > 0) {
-        boolMap[index] = 1;
-    }
-    else {
-        boolMap[index] = 0;
-    }
+function ForEachRemove(item, index, arr) {
+    arr[index] = arr[index].filter(NoNewline);
 }
+
+mapArray.forEach(ForEachRemove);
 */
---
+++ main.js
@@ -0,0 +1,86 @@
+//Used variables
+var rawData;
+var strData = "";
+var numOnly = "";
+var mapArray;
+
+// JavaScript source code
+function ReadIt() {
+
+    //Read data from form, get number values
+    var file = document.getElementById('mapFile').files[0];
+    var reader = new FileReader();
+    reader.onload = () => {
+        strData = event.target.result;
+        DoIt();
+    }
+    reader.readAsText(file);
+}
+
+function DoIt() {
+    //Read numbers from data
+    numOnly = strData.slice(157);
+
+
+    //Turn into 2D string array
+    console.log("Converting to array...");
+    mapArray = numOnly.split("\n")
+    for (let i = 0; i < mapArray.length; i++) {
+        mapArray[i] = mapArray[i].split(" ");
+    }
+    mapArray.pop();
+    console.log("Done.");
+
+
+    //Convert into float
+    console.log("Converting to float...");
+    function IntoFloat(item, index, arr) {
+        arr[index] = parseFloat(item);
+    }
+
+    function DIntoFloat(item, index, arr) {
+        arr[index].forEach(IntoFloat);
+    }
+
+    mapArray.forEach(DIntoFloat);
+    console.log("Done.");
+
+
+    //Write boolMap
+    console.log("Creating boolMap...")
+    var boolMap = new Array(mapArray.length);
+    for (let row = 0; row < mapArray.length; row++) {
+        boolMap[row] = [];
+        for (let column = 0; column < mapArray[row].length; column++) {
+            if (mapArray[row][column] > 0) {
+                boolMap[row][column] = 1;
+            }
+            else {
+                boolMap[row][column] = 0;
+            }
+        }
+        //mapArray[row].push("\n");
+    }
+    console.log("Done.");
+
+
+    //Make canvas
+    console.log("Creating canvas...");
+    var mapCanvas = document.getElementById("mapPicture");
+    var mapContext = mapCanvas.getContext("2d");
+
+    for (let i = 0; i < boolMap.length; i++)
+
+    console.log("Done.");
+    PrintIt();
+}
+
+function PrintIt() {
+    //Print an array to console
+    var targetVar = boolMap;
+    for (let row = 0; row < targetVar.length; row++) {
+        //printOut = boolMap[row].join("");
+
+        console.log(...targetVar[row]); //Direct print (slow?)
+    }
+}
